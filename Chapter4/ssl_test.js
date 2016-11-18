339fca013f53975950f36ccfca001037a75dc48c
exilfrom <exilfrom@gmail.com>
2016-11-18 16:13:53 +0200

    update testApi and last part chapter4

---
+++ Chapter4/ssl_test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ievgen.garlinskyi on 18.11.2016.
+ */
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+    key: fs.readFileSync('./key.pem', 'utf8'),
+    cert: fs.readFileSync('./key-cert.pem', 'utf8')
+};
+
+https.createServer(options, function(req, res){
+    res.writeHead(200);
+    res.end('hello world\n');
+}).listen(3000);

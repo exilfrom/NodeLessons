12e5d025c59328a5b5b65a275221dda23d57f872
exilfrom <exilfrom@gmail.com>
2016-11-19 02:50:51 +0200

    testApi update

--- TestApi/server.js
+++ TestApi/server.js
@@ -4,22 +4,18 @@
 testData = {
     users : [
         {
-            id: 1,
             name: 'Ken',
             email: 'some@mail.tt'
         },
         {
-            id: 2,
             name: 'Jon',
             email: 'jonMail@mail.tt'
         },
         {
-            id: 3,
             name: 'Bob',
             email: 'bob@mail.tt'
         },
         {
-            id: 4,
             name: 'someName',
             email: 'someName@mail.tt'
         }
@@ -27,9 +23,43 @@ testData = {
 }
 
 var rootPath = '/api/users';
+var dbName = 'TestDb';
+var dbAddress = 'mongodb://localhost:27017/';
 
 var http = require('http'),
-    url = require('url');
+    url = require('url'),
+    mongo = require('mongodb');
+
+var client = mongo.MongoClient;
+var urlDb = dbAddress + dbName;
+
+client.connect(urlDb, function(err, db){
+    if(err){
+        console.log('Error: ' + err );
+    } else {
+        var collection = db.collection('users');
+        collection.find({}).toArray(function(err, result){
+            if(err){
+                console.log('Error: ' + err);
+                db.close();
+                return;
+            } else if(result.length){
+                console.log('Collection exist');
+                db.close();
+                return;
+            }else{
+                collection.insert(testData.users, {safe: true}, function(err, result){
+                    if(err){
+                        console.log('Error: ' + err);
+                    }else{
+                        console.log('Default users was added');
+                    }
+                    db.close();
+                });
+            }
+        });
+    }
+});
 
 var server = http.createServer(function(req, res){
     var path = url.parse(req.url).pathname;
@@ -39,18 +69,15 @@ var server = http.createServer(function(req, res){
 
     switch (req.method){
         case 'GET':
-            if( path == rootPath)
-                return sendJsonResponse(res, testData.users);
-
-            var id = parseInt(path.replace(rootPath + '/', ''), 10);
-
-            if(isNaN(id))
-                return badRequest(res, 'Invalid item id');
-
-            var item = testData.users.filter(function(el){ return el.id === id})[0];
-
-            return item ? sendJsonResponse(res, item) : notFound(res, 'Item with this id not found');
-
+            if( path == rootPath){
+                return getItemList(res);
+            } else {
+                var id = path.replace(rootPath + '/', '');
+                if(!isValidObjectID(id)) {
+                    return badRequest(res, 'No valid Id');
+                }
+                return getItem(res, id);
+            }
         default :
             return badRequest(res);
     }
@@ -69,6 +96,59 @@ function notFound(res, message){
 function badRequest(res, message){
     res.statusCode = 400;
     res.end(message ? message :'Bad Request');
+}
+
+function isValidObjectID(str) {
+    return (/^[0-9a-fA-F]{24}$/).test(str);
+}
+
+function getItem(res, id){
+    client.connect(urlDb, function(err, db){
+        if(err){
+            console.log('Error: ' + err );
+            res.statusCode = 500;
+            res.end('Error connection to db');
+        }else{
+             var o_id = new mongo.ObjectID(id);
+             var collection = db.collection('users');
+             collection.findOne({_id: o_id}, function(err, result){
+                 if (err) {
+                     console.log('Error: ' + err );
+                     res.statusCode = 500;
+                     res.end('Error db');
+                     db.close();
+                 } else {
+                     db.close();
+                     return result ? sendJsonResponse(res, result) : notFound(res, 'Item with this id not found');
+                 }
+             });
+        }
+    })
+}
+
+function getItemList(res){
+    client.connect(urlDb, function(err, db){
+        if(err){
+            console.log('Error: ' + err );
+            res.statusCode = 500;
+            res.end('Error connection to db');
+        }else{
+            var collection = db.collection('users');
+            collection.find({}).toArray(function (err, result) {
+                if (err) {
+                    console.log('Error: ' + err );
+                    res.statusCode = 500;
+                    res.end('Error db');
+                } else if (result.length) {
+                    db.close();
+                    return sendJsonResponse(res, result);
+                } else {
+                    res.end('Users List is empty');
+                }
+                db.close();
+            });
+        }
+    })
 }
 
 server.listen(3000);
